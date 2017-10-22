bbe73c77b4146f5580c932128e312630491ec0bd
Konstantin <const.miyang@gmail.com>
2017-10-22 05:01:58 +0200

    Update assembly info

--- Cyriller/Properties/AssemblyInfo.cs
+++ Cyriller/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Cyriller")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Russian language declension library")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("http://www.miyconst.com/")]
 [assembly: AssemblyProduct("Cyriller")]
-[assembly: AssemblyCopyright("Copyright © Konstantin Mi  2015")]
+[assembly: AssemblyCopyright("Copyright © Konstantin Mi 2014-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
